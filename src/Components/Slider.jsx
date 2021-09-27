fb3e2318618de42974cbfa8fbbeb742baf0c5e9b
Wangdi Tenzing <wangdi457@gmail.com>
2021-09-27 18:46:23 -0400

    created a dummy data file dummydata consists of an array of objects exported the array of objects mapped the objects rendering objects from the dummydata now

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -2,6 +2,7 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 
 import React, { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
@@ -34,7 +35,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transform: translateX(0vw);
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -50,6 +51,8 @@ const ImgContainer = styled.div`
 
 const Image = styled.img`
   height: 80%;
+  margin-left: 30%;
+  margin-top: 5%;
 `;
 const InfoContainer = styled.div`
   flex: 1;
@@ -76,54 +79,31 @@ const Button = styled.button`
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
 
-  const handleClick = (direction) => {};
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImgContainer>
-            <Image src="https://cdn.shopify.com/s/files/1/0417/8149/3912/products/wai-wai-chicken.png?v=1608566509" />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>DASAIN AND TIHAR SALE ON ALL TIMES </Title>
-            <Description>
-              Dont Wait !! Purchases above $100 gets a free box of Wai Wai ,
-              above $50 gets a free Tropicana Orange Juice{" "}
-            </Description>
-            <Button> Shop Now !</Button>
-          </InfoContainer>
-        </Slide>
-
-        <Slide>
-          <ImgContainer>
-            <Image src="https://cdn.shopify.com/s/files/1/0417/8149/3912/products/wai-wai-chicken.png?v=1608566509" />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>ALMOST LOSAR </Title>
-            <Description>
-              Dont Wait !! Purchases above $100 gets a free box of Wai Wai ,
-              above $50 gets a free Tropicana Orange Juice{" "}
-            </Description>
-            <Button> Shop Now !</Button>
-          </InfoContainer>
-        </Slide>
-
-        <Slide>
-          <ImgContainer>
-            <Image src="https://cdn.shopify.com/s/files/1/0417/8149/3912/products/wai-wai-chicken.png?v=1608566509" />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>GET READY FOR XMAS </Title>
-            <Description>
-              Dont Wait !! Purchases above $100 gets a free box of Wai Wai ,
-              above $50 gets a free Tropicana Orange Juice{" "}
-            </Description>
-            <Button> Shop Now !</Button>
-          </InfoContainer>
-        </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>{item.desc}</Description>
+              <Button> Shop Now !</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
       <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
