635adfd81b5e7d9440151eafe1508a28251fbc30
Wangdi Tenzing <wangdi457@gmail.com>
2021-09-27 18:14:48 -0400

    added images , descrption and shop now button on the slide component Made style changes to render the elements in the right format Need to add a dummy data for the slides

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,14 +1,15 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
+
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -25,14 +26,106 @@ const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
 `;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(0vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background=color: transparent;
+  cursor: pointer;
+`;
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {};
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Arrow direction="right">
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://cdn.shopify.com/s/files/1/0417/8149/3912/products/wai-wai-chicken.png?v=1608566509" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>DASAIN AND TIHAR SALE ON ALL TIMES </Title>
+            <Description>
+              Dont Wait !! Purchases above $100 gets a free box of Wai Wai ,
+              above $50 gets a free Tropicana Orange Juice{" "}
+            </Description>
+            <Button> Shop Now !</Button>
+          </InfoContainer>
+        </Slide>
+
+        <Slide>
+          <ImgContainer>
+            <Image src="https://cdn.shopify.com/s/files/1/0417/8149/3912/products/wai-wai-chicken.png?v=1608566509" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>ALMOST LOSAR </Title>
+            <Description>
+              Dont Wait !! Purchases above $100 gets a free box of Wai Wai ,
+              above $50 gets a free Tropicana Orange Juice{" "}
+            </Description>
+            <Button> Shop Now !</Button>
+          </InfoContainer>
+        </Slide>
+
+        <Slide>
+          <ImgContainer>
+            <Image src="https://cdn.shopify.com/s/files/1/0417/8149/3912/products/wai-wai-chicken.png?v=1608566509" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>GET READY FOR XMAS </Title>
+            <Description>
+              Dont Wait !! Purchases above $100 gets a free box of Wai Wai ,
+              above $50 gets a free Tropicana Orange Juice{" "}
+            </Description>
+            <Button> Shop Now !</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
