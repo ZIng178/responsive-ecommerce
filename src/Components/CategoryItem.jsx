9b573c3d2108b4b3dd23a7a516ac9e9c6fbdc942
Wangdi Tenzing <wangdi457@gmail.com>
2021-09-28 13:58:53 -0400

    Added styles to the Images Need to create Product Compnent Now

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Title = styled.h1``;
-const Button = styled.button``;
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
+`;
 
 const CategoryItem = ({ item }) => {
   return (
