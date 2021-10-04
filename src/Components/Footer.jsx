fb8f244ceb1e7e9a86176dabb64109b7abb55e8b
Wangdi Tenzing <wangdi457@gmail.com>
2021-10-04 11:04:23 -0400

    Added Color to the Social Icons Passed it as a prop to the Social Icon Component Div Built Title , List and List Item Components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,22 +9,43 @@ const Left = styled.div`
   flex: 1;
 `;
 const Logo = styled.h1``;
-const Desc = styled.p``;
-const SocialContainer = styled.div``;
-const SocialIcon = styled.div``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+const Title = styled.h3``;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
 
 const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo> Hamro Bazaar</Logo>
+        <Logo> MYTHIC</Logo>
         <Desc>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
           perspiciatis dicta quibusdam suscipit consequuntur! Delectus magni
@@ -32,21 +53,34 @@ const Footer = () => {
           necessitatibus! Necessitatibus unde autem ex.
         </Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE     ">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
+      <Center>
+        <Title> Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Mens Fashion</ListItem>
+          <ListItem>Womans Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account </ListItem>
+          <ListItem>Order Traching</ListItem>
+          <ListItem>Wish List</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
       <Right></Right>
     </Container>
   );
