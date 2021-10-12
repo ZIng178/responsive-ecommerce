1695090dcf845cf1b7569ef98a9835de533ca39e
Wangdi Tenzing <wangdi457@gmail.com>
2021-10-12 09:20:18 -0400

    Created Login and Register Pages and added styles to them Need to create a Cart Page now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Product from "./Pages/Product";
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Login />
     </div>
   );
 }
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/3622614/pexels-photo-3622614.jpeg?cs=srgb&dl=pexels-wesley-carvalho-3622614.jpg&fm=jpg")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN </Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="password" />
+
+          <Button>LOGIN</Button>
+          <Link> DO YOU NOT REMEMBER THE PASSWORD ?</Link>
+          <Link> CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
