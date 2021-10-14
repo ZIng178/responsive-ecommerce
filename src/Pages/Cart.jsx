58cdcc72bb29d3ce063d4d1d486b37979fec2c2a
Wangdi Tenzing <wangdi457@gmail.com>
2021-10-14 17:41:20 -0400

    finished cart page need to make some changes to the navbar and also need to make the page responsive

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -42,9 +42,6 @@ const Bottom = styled.div`
 const Info = styled.div`
   flex: 3;
 `;
-const Summary = styled.div`
-  flex: ;
-`;
 
 const Product = styled.div`
 display:flex
@@ -69,7 +66,7 @@ const ProductColor = styled.div`
   border-radius: 50%;
   background-color: ${(props) => props.color};
 `;
-const ProductSize = styled.div``;
+const ProductSize = styled.span``;
 const PriceDetail = styled.div`
   flex: 1;
   display: flex;
@@ -81,6 +78,7 @@ const PriceDetail = styled.div`
 const ProductAmountContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: 20px;
 `;
 const ProductAmount = styled.div`
   font-size: 24px;
@@ -89,6 +87,44 @@ const ProductAmount = styled.div`
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
 `;
 
 const Cart = () => {
@@ -132,11 +168,62 @@ const Cart = () => {
                   <ProductAmount>2</ProductAmount>
                   <Remove />
                 </ProductAmountContainer>
-                <ProductPrice> </ProductPrice>
+                <ProductPrice> $30 </ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C91l1T3IS7kL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX466_.png" />
+                <Details>
+                  <ProductName>
+                    <b>
+                      {" "}
+                      Product:Mickey Mouse Halloween Ghosts Pumpkins Spiders
+                      T-Shirt
+                    </b>
+                  </ProductName>
+                  <ProductId>
+                    {" "}
+                    <b> ID: </b> 32342342342
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    {" "}
+                    <b> Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice> $30 </ProductPrice>
               </PriceDetail>
             </Product>
           </Info>
-          <Summary></Summary>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
