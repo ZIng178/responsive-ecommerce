6506ec4425caa745ef163f9759da270a081786a3
Wangdi Tenzing <wangdi457@gmail.com>
2021-11-05 12:31:08 -0400

    deleted build folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "../public/favicon.ico";
 
 ReactDOM.render(
   <Provider store={store}>
