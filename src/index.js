5078039986ebe1542c4529aa890301e563fbf60d
Wangdi Tenzing <wangdi457@gmail.com>
2021-11-05 01:40:55 -0400

    made changes to the app folder

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "../public/favicon.ico";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -11,3 +12,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+export default App;
